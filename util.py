ff1323ef749c4c3201364cef21077300d2620269
Wei Chen <sysuweichen@gmail.com>
2011-11-25 13:49:49 -0800

    test github

--- util.py
+++ util.py
@@ -5,6 +5,7 @@
 # purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
 # John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+#
 
 import sys
 import inspect
